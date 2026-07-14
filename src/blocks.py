5a5c8e59a8a07108a9d705db7af0a1ed918e015f
JellyMarks <jcmiller1000@gmail.com>
2026-07-14 16:12:27 -0700

    4.2: Block Types

--- src/blocks.py
+++ src/blocks.py
@@ -1,4 +1,15 @@
-def markdown_to_blocks(markdown:str)->list:
+from enum import Enum
+
+
+class BlockType(Enum):
+    PARAGRAPH = "paragraph"
+    HEADING = "heading"
+    CODE = "code"
+    QUOTE = "quote"
+    UNORDERED_LIST = "unordered_list"
+    ORDERED_LIST = "ordered_list"
+
+def markdown_to_blocks(markdown:str) -> list:
     split_markdown = markdown.split("\n\n")
     block = []
     for item in split_markdown:
@@ -7,3 +18,34 @@ def markdown_to_blocks(markdown:str)->list:
             continue
         block.append(stripped_item)
     return block
+
+def block_to_block_type(block:str) -> BlockType:
+    if block.startswith("#"):
+        hash_count = 0
+        for char in block:
+            if char == "#":
+                hash_count += 1
+            else:
+                break
+        if 1 <= hash_count <= 6 and len(block) > hash_count and block[hash_count] == " ":
+            return BlockType.HEADING
+        
+    if block.startswith("```\n") and block.endswith("```"):
+        return BlockType.CODE
+    
+    lines = block.split("\n")
+    expected = 1
+
+    if all(line.startswith(">") for line in lines):
+        return BlockType.QUOTE
+    
+    if all(line.startswith("- ") for line in lines):
+        return BlockType.UNORDERED_LIST
+
+    for line in lines:
+        prefix = f"{expected}. "
+        if line.startswith(prefix):
+            expected += 1
+        else:
+            return BlockType.PARAGRAPH
+    return BlockType.ORDERED_LIST
--- src/test_blocks.py
+++ src/test_blocks.py
@@ -1,7 +1,10 @@
 import unittest
-from blocks import markdown_to_blocks
+from blocks import markdown_to_blocks, block_to_block_type, BlockType
 
 class TestBlocks(unittest.TestCase):
+
+    #MD TO BLOCK TESTS
+
     def test_markdown_to_blocks(self):
         md = """
 This is **bolded** paragraph
@@ -54,6 +57,112 @@ This is the same paragraph on a new line
                 "First block",
                 "Second block",
             ],
+        )
+
+    #BLOCK TO BLOCK TESTS
+
+    def test_block_to_block_type_code(self):
+        md = "```\nThis is a code block\n```"
+        block = block_to_block_type(md)
+        self.assertEqual(
+            block,
+            BlockType.CODE
+        )
+
+    def test_block_to_block_type_bad_code(self):
+        md = "```\nThis is a code block"
+        block = block_to_block_type(md)
+        self.assertEqual(
+            block,
+            BlockType.PARAGRAPH
+        )
+
+    def test_block_to_block_type_quote(self):
+        md = "> 'This is a famous quote block'"
+        block = block_to_block_type(md)
+        self.assertEqual(
+            block,
+            BlockType.QUOTE
+        )
+
+    def test_block_to_block_type_bad_quote(self):
+        md = "> line one\nline two"
+        block = block_to_block_type(md)
+        self.assertEqual(
+            block,
+            BlockType.PARAGRAPH
+        )
+
+    def test_block_to_block_type_unordered_list(self):
+        md = "- This is a list\n- with items"
+        block = block_to_block_type(md)
+        self.assertEqual(
+            block,
+            BlockType.UNORDERED_LIST
+        )
+
+    def test_block_to_block_type_bad_unordered_list(self):
+        md = "- This is a list\n+ with items"
+        block = block_to_block_type(md)
+        self.assertEqual(
+            block,
+            BlockType.PARAGRAPH
+        )
+
+    def test_block_to_block_type_paragraph(self):
+        md = "This is a normal paragraph"
+        block = block_to_block_type(md)
+        self.assertEqual(
+            block,
+            BlockType.PARAGRAPH
+        )
+
+    def test_block_to_block_type_heading(self):
+        md = "###### THIS IS A HEADING"
+        block = block_to_block_type(md)
+        self.assertEqual(
+            block,
+            BlockType.HEADING
+        )
+
+    def test_block_to_block_type_long_heading(self):
+        md = "####### THIS IS A BAD HEADING"
+        block = block_to_block_type(md)
+        self.assertEqual(
+            block,
+            BlockType.PARAGRAPH
+        )
+
+    def test_block_to_block_type_bad_heading(self):
+        md = "#Not a heading"
+        block = block_to_block_type(md)
+        self.assertEqual(
+            block,
+            BlockType.PARAGRAPH
+        )
+
+    def test_block_to_block_type_ordered_list(self):
+        md = "1. This is an ordered list\n2. with ordered items"
+        block = block_to_block_type(md)
+        self.assertEqual(
+            block,
+            BlockType.ORDERED_LIST
+        )
+
+    def test_block_to_block_type_bad_ordered_list(self):
+        md = "1. This is an ordered list\n4. with unordered items"
+        block = block_to_block_type(md)
+        self.assertEqual(
+            block,
+            BlockType.PARAGRAPH
+        )
+
+    def test_block_to_block_type_worse_ordered_list(self):
+        md = "2. nope\n3. nope"
+        block = block_to_block_type(md)
+        self.assertEqual(
+            block,
+            BlockType.PARAGRAPH
         )
 
 if __name__ == "__main__":
