90c6daa48fe87b4653eb451442741a8c4773f500
JellyMarks <jcmiller1000@gmail.com>
2026-08-11 12:47:05 -0700

    5.5.1: Build and Shell

--- src/main.py
+++ src/main.py
@@ -1,16 +1,21 @@
-import os, shutil
+import os, shutil, sys
 from os import path
 from blocks import markdown_to_html_node
 
 
 def main():
+    try:
+        basepath = sys.argv
+    except:
+        basepath = "/"
+
     destination = path.abspath("public")
     source = path.abspath("static")
     content = path.abspath("content")
     if os.path.isdir(destination):
         shutil.rmtree(destination)
     copy_to_and_from_dir(source, destination)
-    generate_pages_recursive(content, "template.html", destination)
+    generate_pages_recursive(content, "template.html", destination, basepath)
 
 def copy_to_and_from_dir(source, destination):
     files = os.listdir(source)
@@ -52,7 +57,7 @@ def extract_title(markdown):
     else:
         raise Exception ("Missing Title")
 
-def generate_page(from_path, template_path, dest_path):
+def generate_page(from_path, template_path, dest_path, basepath):
     print (f"Generating page from {from_path} to {dest_path} using {template_path}")
     with open(from_path, "r") as file:
         from_path_md = file.read()
@@ -63,12 +68,14 @@ def generate_page(from_path, template_path, dest_path):
     title = extract_title(from_path_md)
     template_title = template_path_template.replace("{{ Title }}", title)
     template_replaced = template_title.replace("{{ Content }}", html)
+    href_replaced = template_replaced.replace('href="/', f'href="{basepath}')
+    src_replaced = href_replaced.replace('href="/', f'href="{basepath}')
     directories_needed = os.path.dirname(dest_path)
     os.makedirs(directories_needed, exist_ok=True)
     with open(dest_path, "w") as file:
-                file.write(template_replaced)
+                file.write(src_replaced)
 
-def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
+def generate_pages_recursive(dir_path_content, template_path, dest_dir_path, basepath):
     files = os.listdir(dir_path_content)
     
     for filename in files:
@@ -83,8 +90,8 @@ def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
             destination_file = os.path.join(dest_dir_path, filename)
 
         if os.path.isfile(current_file):
-            generate_page(current_file, template_path, destination_file)
+            generate_page(current_file, template_path, destination_file, basepath)
         else:
-            generate_pages_recursive(current_file, template_path, destination_file)
+            generate_pages_recursive(current_file, template_path, destination_file, basepath)
 
 main()
